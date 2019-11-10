c44f76a18e7d193f91875efda556d56c20a98a48
Gabriel Sarubo <gabrielsarubo@hotmail.com>
2019-11-10 00:31:10 -0300

    Remove a todo item

--- src/App.js
+++ src/App.js
@@ -8,12 +8,22 @@ class App extends React.Component {
       {id: 2, content: 'play mario kart'}
     ]
   }
+
+  deleteTodo = id => {
+    const todos = this.state.todos.filter(todo => {
+      return id !== todo.id
+    })
+
+    this.setState({
+      todos: todos
+    })
+  }
   
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos} />
+        <Todos deleteTodo={this.deleteTodo} todos={this.state.todos} />
       </div>
     );
   }
