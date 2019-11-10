9ce974395617bb9cbecd2ec1e9c20ab7e324fea4
Gabriel Sarubo <gabrielsarubo@hotmail.com>
2019-11-10 00:28:03 -0300

    Add Todos component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import './App.css';
+import Todos from './Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'play mario kart'}
+    ]
+  }
+  
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
